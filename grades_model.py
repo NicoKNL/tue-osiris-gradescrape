edfb6df298595f746e90ae8dd5c8a9073d281e5b
Nico <klaassen.nico@gmail.com>
2018-04-28 18:09:44 +0200

    row and column order  + cleaning up the scraped input data

--- grades_model.py
+++ grades_model.py
@@ -1,24 +1,26 @@
-from PyQt5.QtCore import QAbstractTableModel, Qt
+from PyQt5.QtCore import QAbstractTableModel, Qt, QVariant
 
 
 class GradesModel(QAbstractTableModel):
-    def __init__(self, grades_data, header_data):
+    def __init__(self, grades_data):
         super(QAbstractTableModel, self).__init__()
         self.grades_data = grades_data
-        self.header_data = header_data
 
     def rowCount(self, parent=None, *args, **kwargs):
-        return len(self.grades_data)
+        return len(self.grades_data[0]) - 1
 
     def columnCount(self, parent=None, *args, **kwargs):
-        return len(self.grades_data[0])
-        #return len(self.header_data)
+        return len(self.grades_data)
+        # return len(self.header_data)
 
     def data(self, index, role=Qt.DisplayRole):
-        return self.grades_data[index.row()][index.column()]
+        if (role == Qt.DisplayRole):
+            return self.grades_data[index.column()][index.row() + 1]
+        else:
+            return QVariant()
 
     def headerData(self, section, orientation, role=Qt.DisplayRole):
         if role == Qt.DisplayRole and orientation == 1:
-            return self.header_data[section]
-            #return self.header_data[section]
+            return self.grades_data[section][0]
+            # return self.header_data[section]
         return QAbstractTableModel.headerData(self, section, orientation, role)
--- gradescrape.py
+++ gradescrape.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from bs4 import BeautifulSoup
 from browser import Render
+from pprint import pprint
 
 this_folder = os.path.dirname(os.path.realpath(__file__))
 data_file = f"{this_folder}/gradescrape.json"
@@ -39,24 +40,26 @@ grade_table = soup.find_all('table', {'class': 'OraTableContent'})[0]
 table_headers = grade_table.findChildren('th')
 print(table_headers)
 
-grade_data = []
+header_data = []
 for header in table_headers:
     header_text = header.get_text()
-    grade_data.append(header_text)
+    header_data.append(header_text)
 
-print(grade_data)
+print(header_data)
 
 rows = grade_table.findChildren('tr')
 
-grade_data = [grade_data] # Nest the header entries
-for row in rows:
-    cols = row.findChildren('td')
-    row_data = []
-    for col in cols:
-        row_data.append(col.get_text())
+grade_data = [[header] for header in header_data] # Nest the header entries
 
-    if row_data:
-        grade_data.append(row_data) #append(" - ".join([d for d in row_data if d]))
+for i, row in enumerate(rows):
+    cols = row.findChildren('td')
+    # row_data = []
+    for column_idx, col in enumerate(cols):
+        grade_data[column_idx].append(col.get_text())
+        # row_data.append(col.get_text())
+
+    # if row_data:
+    #     grade_data.append(row_data) #append(" - ".join([d for d in row_data if d]))
 
 try:
     previous_data = json.load(open(data_file))
@@ -73,6 +76,52 @@ for gd in grade_data:
             break
     if not old_news:
         new_grades.append(gd)
+
+def all_to_string(input_list):
+    for i, column in enumerate(input_list):
+        for j, row in enumerate(input_list[i]):
+            input_list[i][j] = str(input_list[i][j])
+    return input_list
+
+def is_list_rectangular(input_list):
+    column_length = None
+
+    for row_idx in range(len(input_list)):
+        if row_idx == 0:
+            column_length = len(input_list[row_idx])
+            continue
+
+        if len(input_list[row_idx]) != column_length:
+            return False
+
+    return True
+
+
+def remove_empty_columns(input_list):
+    import numpy as np
+    print(np.matrix(input_list))
+    print("\n")
+    empty_columns = []
+
+    for column_idx in range(len(input_list)):
+        is_empty = True
+        for row_idx in range(len(input_list[column_idx])):
+            print(f"- {input_list[column_idx][row_idx]}")
+            if input_list[column_idx][row_idx]:
+                is_empty = False
+                # break
+
+        if is_empty:
+            empty_columns.append(column_idx)
+
+    for column_idx in reversed(empty_columns):
+        input_list.pop(column_idx)
+
+    print(np.matrix(input_list))
+    return input_list
+
+grade_data = all_to_string(grade_data)
+grade_data = remove_empty_columns(grade_data)
 
 # Export the grades to a local datafile for future use
 with open(data_file, 'w') as f:
@@ -109,8 +158,9 @@ if True:
 
             print(dir(self))
             self.grades_data = json.load(open(data_file))
-            self.grades_model = GradesModel(self.grades_data[1:], self.grades_data[0])
+            self.grades_model = GradesModel(self.grades_data)
             self.grades_view.setModel(self.grades_model)
+
 
     if __name__ == "__main__":
         # app = QtWidgets.QApplication(sys.argv)
